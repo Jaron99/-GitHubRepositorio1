91c40733cc6456a1ac074ef92e61bac3f97aedcb
Jaron Aviles <jaron.aviles@gmail.com>
2025-05-18 21:03:16 -0600

    Se añadio archivo gitignore

--- obj/Debug/net6.0/ColecDictionary.AssemblyInfo.cs
+++ obj/Debug/net6.0/ColecDictionary.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ColecDictionary")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d0bd40bd922dde87206bfebc18cc5da7a2408476")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+fda25a86e0dc5e96124d968bbf4cd837192e2d4e")]
 [assembly: System.Reflection.AssemblyProductAttribute("ColecDictionary")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ColecDictionary")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
